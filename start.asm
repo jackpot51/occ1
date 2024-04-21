71d2daedd30932ea953da1e71f79b97d0b152373
Jeremy Soller <jackpot51@gmail.com>
2024-04-20 21:11:38 -0600

    Simplify start.asm

--- start.asm
+++ start.asm
@@ -1,12 +1,6 @@
-.module start
-.globl _main
-
-.area _START (ABS)
+.area _HEADER (ABS)
 .org 0x100
-start:
-    ld sp, #0xC800  ; Set stack pointer to 50 KiB
-    call _main      ; Call main function
-    rst 0           ; Exit program
-
-.area _CODE
-.area _DATA
+.globl _main
+ld sp, #0xC800  ; Set stack pointer to 50 KiB
+call _main      ; Call main function
+rst 0           ; Exit program
